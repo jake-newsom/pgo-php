3dc77537d16192a53e26647c9b05ae7cbab75e22
Jake Newsom <newsomjk@gmail.com>
2016-07-22 19:01:11 -0400

    Added Trainer Profile logic to controller
    hopefully should be able to pull trainer profile from an endpoint. Still
    just set to the trainer info in your env settings

--- app/Http/Controllers/TrainerController.php
+++ app/Http/Controllers/TrainerController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -8,14 +7,27 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
+use App\Models\Http\Client;
+
 class TrainerController extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 
     public function index(){
-    	//update this once you figure out what code populates the trainer profile
-    	$trainer = array("testuser"=>"testvalue");
 
-    	return response()->json($trainer);
+        $service = env('AUTH_SERVICE');
+        $username = env('AUTH_USERNAME');
+
+        $client = new Client;
+        $accessToken = $client->accessToken;
+        $apiUrl = $client->apiEndpoint;
+
+		$trainer = new Trainer($client);
+        $profile = $trainer->profile();
+
+    	//update this once you figure out what code populates the trainer profile
+    	//$trainer = array("testuser"=>"testvalue");
+
+    	return response()->json($profile);
     }
 }
